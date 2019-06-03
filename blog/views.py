8d4601e48c97ab6672bde8127fa3376fae1e7b31
kerolos <kerolos.sss@gmail.com>
2019-06-03 19:13:19 +0200

    securing content creation

--- blog/views.py
+++ blog/views.py
@@ -13,6 +13,10 @@ def post_detail(request, pk):
     return render(request, 'blog/post_detail.html', {'post': post})
 
 def post_new(request):
+    if not request.user.is_authenticated:
+        return redirect('post_list')
+        
+
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
@@ -26,6 +30,9 @@ def post_new(request):
     return render(request, 'blog/post_edit.html', {'form': form})
 
 def post_edit(request, pk):
+    if not request.user.is_authenticated:
+        return redirect('post_list')
+        
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
         form = PostForm(request.POST, instance=post)
